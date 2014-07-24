9ba48b791ef038b39b9e36f053d1050c043be374
PEMapModder <pemapmodder1970@gmail.com>
2014-07-24 15:02:57 +0800

    Finished temp ban with RegExp again :D

--- src/EssentialsPE/Commands/TempBan.php
+++ src/EssentialsPE/Commands/TempBan.php
@@ -26,15 +26,23 @@ class TempBan extends BaseCommand{
             $sender->sendMessage(TextFormat::RED . "[Error] Player not found");
             return false;
         }
-        $seconds = 0; // TODO
+        $seconds = 0;
         while(preg_match("#\\A([0-9\\.]{1,}(y|mo|w|d|h|m|s)\\Z#i", array_shift($args), $match)){
             $match = $match[0]; // TODO check if the flag is wrong
             if(!is_numeric($match[1])){
                 break;
             }
+            $unit = 1:
             switch(strtolower($match[2])){
-                // TODO
+                case "m": $unit = 60; break;
+                case "h": $unit = 60 * 60; break;
+                case "d": $unit = 60 * 60 * 24; break;
+                case "w": $unit = 60 * 60 * 24 * 7; break;
+                case "mo": $unit = 60 * 60 * 24 * ((int) date("t")); break; // never knew I were to use "t" ;)
+                case "y": $unit = 60 * 60 * 24 * (date("L") === "1" ? 366:365); break;
             }
+            $amplifier = floatval($match[1]);
+            $seconds += $amplifier * $unit;
         }
 //        $reason = ""; // TODO
         $date = new \DateTime;
