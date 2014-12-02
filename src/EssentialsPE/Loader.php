33f5021030b4662583fc95e34adbf3fe8b9da80c
PEMapModder <pemapmodder1970@gmail.com>
2014-12-02 19:01:56 +0800

    logic change to getSession()

--- src/EssentialsPE/Loader.php
+++ src/EssentialsPE/Loader.php
@@ -488,7 +488,7 @@ class Loader extends PluginBase{
      * @return bool
      */
     public function getSession(Player $player, $key){
-        if(!(isset($this->sessions[$player->getName()]) || isset($this->sessions[$player->getName()][$key]))){
+        if(!isset($this->sessions[$player->getName()]) or !isset($this->sessions[$player->getName()][$key])){
             return false;
         }
         return $this->sessions[$player->getName()][$key];
