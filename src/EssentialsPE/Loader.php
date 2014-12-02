e59bdc6f06c3b387ecd140df1041c0222a77791a
Jorge González <iksaku@me.com>
2014-12-02 06:36:20 -0600

    Update Loader.php

--- src/EssentialsPE/Loader.php
+++ src/EssentialsPE/Loader.php
@@ -473,7 +473,7 @@ class Loader extends PluginBase{
      * @return bool
      */
     public function setSession(Player $player, $key, $value){
-        if(!isset($this->sessions[$player->getName()]) || isset($this->sessions[$player->getName()][$key])){
+        if(!isset($this->sessions[$player->getName()]) || !isset($this->sessions[$player->getName()][$key])){
             return false;
         }
         $this->sessions[$player->getName()][$key] = $value;
