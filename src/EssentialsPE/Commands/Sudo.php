52f4afada21da88833f6ff68f17201e9b1b210da
Jorge González <iksaku@me.com>
2014-12-28 11:20:04 -0600

    More features for 'Sudo' :3

--- src/EssentialsPE/Commands/Sudo.php
+++ src/EssentialsPE/Commands/Sudo.php
@@ -4,6 +4,7 @@ namespace EssentialsPE\Commands;
 use EssentialsPE\BaseCommand;
 use EssentialsPE\Loader;
 use pocketmine\command\CommandSender;
+use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
 
@@ -25,9 +26,22 @@ class Sudo extends BaseCommand{
         if(!$player){
             $sender->sendMessage(TextFormat::RED . "[Error] Player not found");
             return false;
+        }elseif($player->hasPermission("essentials.sudo.exempt")){
+            $sender->sendMessage(TextFormat::RED . "[Error] " . $name . " cannot be sudo'ed");
+            return false;
         }
-        $sender->sendMessage(TextFormat::AQUA . "Command ran has " . $name);
-        $this->getPlugin()->getServer()->dispatchCommand($player, implode(" ", $args));
+
+        $v = implode(" ", $args);
+        if(substr($v, 0, 2) === "c:"){
+            $sender->sendMessage(TextFormat::GREEN . "Sending message as " . $name);
+            $this->getPlugin()->getServer()->getPluginManager()->callEvent($ev = new PlayerChatEvent($player, $v));
+            if(!$ev->isCancelled()){
+                $this->getPlugin()->getServer()->broadcastMessage(\sprintf($ev->getFormat(), $ev->getPlayer()->getDisplayName(), $ev->getMessage()), $ev->getRecipients());
+            }
+        }else{
+            $sender->sendMessage(TextFormat::AQUA . "Command ran has " . $name);
+            $this->getPlugin()->getServer()->dispatchCommand($player, $v);
+        }
         return true;
     }
 } 
