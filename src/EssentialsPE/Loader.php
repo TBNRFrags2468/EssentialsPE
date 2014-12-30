489b9241df099e67447a7c3905f2b4f3cc2ec9a3
Jorge González <iksaku@me.com>
2014-12-29 21:33:54 -0600

    Little change

--- src/EssentialsPE/Loader.php
+++ src/EssentialsPE/Loader.php
@@ -306,7 +306,7 @@ class Loader extends PluginBase{
      *
      * @param string $message
      * @param null $player
-     * @return mixed
+     * @return bool|string
      */
     public function colorMessage($message, $player = null){
         $search = ["&0","&1","&2","&3","&4","&5","&6","&7","&8","&9","&a", "&b", "&c", "&d", "&e", "&f", "&k", "&l", "&m", "&n", "&o", "&r"];
@@ -473,7 +473,7 @@ class Loader extends PluginBase{
      * @param Player $player
      * @return BaseSession
      */
-    public function getSession(Player $player){
+    private function getSession(Player $player){
         if(!$this->sessionExists($player)){
             $this->createSession($player);
         }
