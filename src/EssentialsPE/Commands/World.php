a642c183f61f13ec58ece24ab5b172e64fac4d91
Jorge González <iksaku@me.com>
2014-12-30 00:24:53 -0600

    Optimized 'World' command code

--- src/EssentialsPE/Commands/World.php
+++ src/EssentialsPE/Commands/World.php
@@ -21,27 +21,25 @@ class World extends BaseCommand{
             $sender->sendMessage(TextFormat::RED . "Please run this command in-game");
             return false;
         }
-        switch(count($args)){
-            case 1:
-                $world = $args[0];
-                if(!$sender->getServer()->isLevelGenerated($world)){
-                    $sender->sendMessage(TextFormat::RED . "[Error] World doesn't exists");
-                    return false;
-                }elseif(!$sender->getServer()->isLevelLoaded($world)){
-                    $sender->sendMessage(TextFormat::YELLOW . "Level is not loaded yet. Loading...");
-                    if(!$sender->getServer()->loadLevel($world)){
-                        $sender->sendMessage(TextFormat::RED . "[Error] The level couldn't be loaded");
-                        return false;
-                    }
-                }
-                $world = $sender->getServer()->getLevelByName($world);
-                $sender->teleport($world->getSpawnLocation(), 0, 0);
-                $sender->sendMessage(TextFormat::YELLOW . "Teleporting...");
-                break;
-            default:
-                $sender->sendMessage(TextFormat::RED . $this->getUsage());
-                break;
+        if(count($args) !== 1){
+            $sender->sendMessage(TextFormat::RED . $this->getUsage());
         }
+        if(!$sender->hasPermission("essentials.worlds.*") || !$sender->hasPermission("essentials.worlds." . strtolower($args[0]))){
+            $sender->sendMessage(TextFormat::RED . "[Error] You can't teleport to this world.");
+        }
+        if(!$sender->getServer()->isLevelGenerated($args[0])){
+            $sender->sendMessage(TextFormat::RED . "[Error] World doesn't exists");
+            return false;
+        }elseif(!$sender->getServer()->isLevelLoaded($args[0])){
+            $sender->sendMessage(TextFormat::YELLOW . "Level is not loaded yet. Loading...");
+            if(!$sender->getServer()->loadLevel($args[0])){
+                $sender->sendMessage(TextFormat::RED . "[Error] The level couldn't be loaded");
+                return false;
+            }
+        }
+        $world = $sender->getServer()->getLevelByName($args[0]);
+        $sender->teleport($world->getSpawnLocation(), 0, 0);
+        $sender->sendMessage(TextFormat::YELLOW . "Teleporting...");
         return true;
     }
 } 
