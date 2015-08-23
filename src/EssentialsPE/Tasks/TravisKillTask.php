41bf3c601d1076a1948c4218b58c5c80b88e0367
Falkirks <me@falkirks.com>
2015-08-23 11:17:46 -0700

    First try at Travis

---
+++ src/EssentialsPE/Tasks/TravisKillTask.php
@@ -0,0 +1,19 @@
+<?php
+namespace EssentialsPE\Tasks;
+
+
+use pocketmine\scheduler\PluginTask;
+
+class TravisKillTask extends PluginTask{
+    /**
+     * Actions to execute when run
+     *
+     * @param $currentTick
+     *
+     * @return void
+     */
+    public function onRun($currentTick){
+        $this->getOwner()->getServer()->forceShutdown();
+    }
+
+}
